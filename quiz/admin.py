a709c83067a066433bda720bdd5a5d8db0b7fdda
TanweerAlam <tanweeralam1312@gmail.com>
2019-10-12 19:22:20 +0530

    Quiz model added

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Question, Category, SubCategory, Difficulty
 # Register your models here.
+
+admin.site.register(Question)
+admin.site.register(Category)
+admin.site.register(SubCategory)
+admin.site.register(Difficulty)
--- quiz/models.py
+++ quiz/models.py
@@ -1,9 +1,122 @@
 from django.db import models
 
+from model_utils.managers import InheritanceManager
+
 # Create your models here.
 class Quiz(models.Model):
-	pass
 
+	title = models.CharField(
+		verbose_name=('Title'),
+		max_length=60, blank=False)
+
+	description = models.TextField(
+		verbose_name=('Description'),
+		blank=True, help_text=('a description of the quiz'))
+
+	url = models.SlugField(
+		max_length=60, blank=False,
+		verbose_name=('user friendly url'),
+		help_text=('a user friendly url'))
+
+	category = models.ForeignKey(
+		Category, null=True, blank=True,
+		verbose_name=('Category'), on_delete=models.CASCADE)
+
+	random_order = models.BooleanField(
+		default=False, blank=False,
+		verbose_name=('Random Order'),
+		help_text=_("Display the questions in "
+                    "a random order or as they "
+                    "are set?"))
+
+	max_questions = models.PositiveIntegerField(
+		default=10, blank=True,
+		verbose_name=('Max Questions'),
+		help_text=_("Number of questions to be answered on each attempt."))
+
+	answers_at_end = models.BooleanField(
+		default=True, blank=False,
+		help_text=_("Correct answer is NOT shown after question."
+                    " Answers displayed at the end."),
+        verbose_name=_("Answers at end"))
+
+	exam_paper = models.BooleanField(
+		default=False, blank=False,
+		help_text=_("If yes, the result of each"
+                    " attempt by a user will be"
+                    " stored. Necessary for marking."),
+        verbose_name=_("Exam Paper"))
+
+	single_attempt = models.BooleanField(
+		blank=False, default=False,
+		help_text=_("If yes, only one attempt by"
+                    " a user will be permitted."
+                    " Non users cannot sit this exam."),
+        verbose_name=_("Single Attempt"))
+
+	pass_mark = models.SmallIntegerField(
+		blank=True, default=0,
+		verbose_name=_("Pass Mark"),
+        help_text=_("Percentage required to pass exam."),
+        validators=[MaxValueValidator(100)])
+
+	success_text = models.TextField(
+		blank=True, help_text=_("Displayed if user passes."),
+        verbose_name=_("Success Text"))
+
+	fail_text = models.TextField(
+		verbose_name=_("Fail Text"),
+        blank=True, help_text=_("Displayed if user fails."))
+
+	draft = models.BooleanField(
+		blank=True, default=False,
+        verbose_name=_("Draft"),
+        help_text=_("If yes, the quiz is not displayed"
+                    " in the quiz list and can only be"
+                    " taken by users who can edit"
+                    " quizzes."))
+
+	def save(self, force_insert=False, force_update=False, *args, **kwargs):
+		self.url = re.sub('\s+', '-', self.url).lower()
+
+		self.url = ''.join(letter for letter in self.url if 
+							letter.isalnum() or letter == '-')
+
+		if self.single_attempt is True:
+			self.exam_paper = True
+
+		if self.pass_mark > 100:
+			raise ValidationError('%s is above 100' % self.pass_mark)
+
+		super(Quiz, self).save(force_insert, force_update, *args, **kwargs)
+
+	class Meta:
+		verbose_name=('Quiz')
+		verbose_name_plural=('Quizzes')
+
+
+	def __str__(self):
+		return self.title
+
+
+	def get_questions(self):
+		return self.question_set.all().select_subclasses()
+
+	@property
+	def get_max_score(self):
+		return self.get_questions().count()
+
+	# Details of anonymous user
+
+	def anon_score_id(self):
+		return str(self.id)+'_score'
+
+	def anon_q_list(self):
+		return str(self.id)+'_q_list'
+
+	def anon_q_data(self):
+		return str(self.id)+'_q_data'
+	
 
 class CategoryManager(models.Manager):
 	def new_category(self, category):
@@ -51,8 +164,9 @@ class SubCategory(models.Model):
 	def __str__(self):
 		return self.sub_category + "(" + self.category.category + ")"
 
-# class DifficultyManager(models.Manager):
-	
+class DifficultyManager(models.Manager):
+ 	pass
+
 
 class Difficulty(models.Model):
 
@@ -64,7 +178,12 @@ class Difficulty(models.Model):
 
 	objects = DifficultyManager()
 
+	class Meta:
+		verbose_name=('Difficulty')
 
+
+	def __str__(self):
+		return self.difficulty
 
 class Question(models.Model):
 	"""
@@ -82,7 +201,7 @@ class Question(models.Model):
 								 verbose_name=('Category'),
 								 on_delete=models.CASCADE)
 
-	sub_category = models.ForeignKey(Sub_category,
+	sub_category = models.ForeignKey(SubCategory,
 									 blank=False,
 									 null=False,
 									 verbose_name=('Sub-Category'),
